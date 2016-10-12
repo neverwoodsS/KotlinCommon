911d35785d1ead2cbc826f578f41922a81852979
张璐琳 <413700858@qq.com>
2016-10-12 11:52:48 +0800

    新增部分扩展函数

---
+++ base/src/main/java/com/zll/kotlin/base/BlockExtension.kt
@@ -0,0 +1,27 @@
+package com.zll.hellokotlin.extension
+
+import android.os.Build
+import com.zll.kotlin.base.BuildConfig
+
+/**
+ * Created by zhangll on 16/9/26.
+ */
+inline fun supportsLollipop(code: () -> Unit) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        code()
+    }
+}
+
+inline fun debug(code: () -> Unit) {
+    if (BuildConfig.DEBUG) {
+        code()
+    }
+}
+
+inline fun handleException(code : () -> Unit) {
+    try {
+        code()
+    } catch (e : Exception) {
+        e.printStackTrace()
+    }
+}
---
+++ base/src/main/java/com/zll/kotlin/base/LogExtension.kt
@@ -0,0 +1,29 @@
+package com.zll.hellokotlin.extension
+
+import android.util.Log
+import java.util.concurrent.locks.Lock
+
+/**
+ * Created by zhangll on 16/9/26.
+ */
+inline fun <reified T> T.log(log: Any) {
+    debug {
+        Log.i(T::class.simpleName, log.toString())
+    }
+}
+
+inline fun <reified T> T.warn(log: Any) {
+    debug {
+        Log.w(T::class.simpleName, log.toString())
+    }
+}
+
+inline fun <reified T> T.error(log: Any) {
+    debug {
+        Log.e(T::class.simpleName, log.toString())
+    }
+}
+
+inline fun <T, R : Lock> T.lock(r : R, code: () -> Unit) {
+
+}
